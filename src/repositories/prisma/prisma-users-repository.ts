ad8ec4b2508cff5a0101c9555fbbeee02677c9e9
Simão Neto <simao.neto@capgemini.com>
2023-09-11 21:04:11 +0100

    feat: Implementation of prisma-check-ins-repository.ts prisma-users-repository.ts

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,6 +4,15 @@ import { UsersRepository } from "../users-repository";
 
 
 export class PrismaUsersRepository implements UsersRepository {
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+        })
+        return user
+    }
+
     async create(data: Prisma.UserCreateInput) {
         const user = await prisma.user.create({
             data,
