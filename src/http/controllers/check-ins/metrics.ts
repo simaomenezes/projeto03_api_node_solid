db51c97aa9da7381c07c3dc5549a41b63f63ecd2
Simão Neto <simaomenezesneto@gmail.com>
2023-09-15 22:54:55 +0100

    feat: Create controllers checkIn and routes

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,15 @@
+import { makeGetUserMetricUseCase } from "@/user-cases/factories/make-get-user-metrics-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
+    const getUserMetricUseCase = makeGetUserMetricUseCase()
+
+    const { checkInsCount } = await getUserMetricUseCase.execute({
+        userId: request.user.sub,
+    })
+
+    return reply.status(200).send({
+        checkInsCount,
+    })
+
+}
