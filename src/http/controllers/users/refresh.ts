c9030721cf000da4b3f35036c5d78c99b3e27c9e
Simão Neto <simaomenezesneto@gmail.com>
2023-09-16 15:21:25 +0100

    feat: Create refresh token

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,37 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+
+    await request.jwtVerify({ onlyCookie: true })
+
+    let token = null
+    let refreshToken = null
+    token = await reply.jwtSign(
+        {}, 
+            {
+                sign: {
+                    sub: request.user.sub,
+                },
+            },
+        )
+    refreshToken = await reply.jwtSign(
+        {}, 
+            {
+                sign: {
+                    sub: request.user.sub,
+                    expiresIn: '7d',
+                },
+            },
+        )
+    
+    return reply
+        .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true,
+            sameSite: true,
+            httpOnly: true,            
+        })
+        .status(200).send({
+            token,
+        })
+}
